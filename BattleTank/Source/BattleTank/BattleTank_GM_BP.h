e9798ea39083634858b8da8016a79491094a1f92
ZoomZoomin <jakeheintz@gmail.com>
2019-04-15 21:40:56 -0400

    End of Lesson 134 - Fixed Bug with Input, Azimuth and Rotation were swapped

---
+++ BattleTank/Source/BattleTank/BattleTank_GM_BP.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "BattleTank_GM_BP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ABattleTank_GM_BP : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ BattleTank/Source/BattleTank/TankGameMode_BP1.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "TankGameMode_BP1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankGameMode_BP1 : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
